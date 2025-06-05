73fcfefca75b6d6f3f262697e5f26c78da32bd2f
deepanm87 <deepan.murugan29@gmail.com>
2025-06-04 21:04:53 -0400

    created use page state and app state context

--- src/Node/BasicNode.tsx
+++ src/Node/BasicNode.tsx
@@ -2,20 +2,19 @@ import type { NodeData } from "../utils/types"
 import styles from "./Node.module.css"
 import { useRef, useEffect, FormEventHandler, KeyboardEventHandler } from "react"
 import { nanoid } from "nanoid"
+import { useAppState } from "../state/AppStateContext"
 
 type BasicNodeProps = {
     node: NodeData
     updateFocusedIndex(index: number): void
     isFocused: boolean
     index: number
-    addNode(node: NodeData, index: number): void
-    removeNodeByIndex(index: number): void
-    changeNodeValue(index: number, value: string): void
 }
 
-export const BasicNode = ({ node, updateFocusedIndex, isFocused, index, addNode, removeNodeByIndex, changeNodeValue }: BasicNodeProps) => {
+export const BasicNode = ({ node, updateFocusedIndex, isFocused, index }: BasicNodeProps) => {
 
     const nodeRef = useRef<HTMLDivElement>(null)
+    const { changeNodeValue, removeNodeByIndex, addNode } = useAppState()
 
     useEffect( () => {
         if(isFocused){
