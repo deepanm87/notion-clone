8bdfa753b6da830601da482ed1e0e918ac57fce3
deepanm87 <deepan.murugan29@gmail.com>
2025-06-05 19:00:03 -0400

    implemented authentication via magic link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import Page from "./Page/Page"
 import { AppStateProvider } from "./state/AppStateContext"
 import { createPage } from "./utils/createPage"
+import { Route, Routes } from "react-router-dom"
+import { Auth } from "./auth/Auth"
+import { Private } from "./auth/Private"
 
 const initialState = createPage()
 
 function App() {
   return (
-    <AppStateProvider initialState={initialState}>
-      <Page />
-    </AppStateProvider>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/:id" element={
+        <Private component={<AppStateProvider initialState={initialState}>
+            <Page />
+          </AppStateProvider>} />
+          
+        } />
+      <Route 
+        path="/"
+        element={
+          <Private 
+            component={<AppStateProvider initialState={initialState}>
+            <Page />
+          </AppStateProvider>} />
+        }
+      />
+    </Routes>
+    
   )
 }
 
---
+++ src/auth/AuthSessionContext.tsx
@@ -0,0 +1,45 @@
+import { supabase } from "../supabaseClient"
+import type { Session } from "@supabase/supabase-js"
+import type { ReactNode } from "react"
+import { createContext, useState, useEffect, useContext } from "react"
+
+type AuthSessionContextValue = {
+    session: Session | null
+    loading: boolean
+}
+
+const AuthSessionContext = createContext<AuthSessionContextValue>({} as AuthSessionContextValue)
+
+type AuthSessionProviderProps = {
+    children: ReactNode
+}
+
+export const AuthSessionProvider = ({children}: AuthSessionProviderProps) => {
+    const [session, setSession] = useState<Session>(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect( () => {
+        const auth = async () => {
+            const { data, error } = await supabase.auth.getSession()
+            if(data.session) {
+                setSession(data.session)
+                setLoading(false)
+            } else {
+                console.log(error)
+            }
+        }
+        auth()
+        supabase.auth.onAuthStateChange( (event, session) => {
+            setSession(session)
+            setLoading(false)
+        })
+    }, [])
+
+    return(
+        <AuthSessionContext.Provider value={{ session, loading }}>
+            {children}
+        </AuthSessionContext.Provider>
+    )
+}
+
+export const useAuthSession = () => useContext(AuthSessionContext)
