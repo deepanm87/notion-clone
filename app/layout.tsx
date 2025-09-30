f4d7752fd200f832fb261e6e9789656c323991be
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-30 16:12:01 -0400

    created header component with clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Notion Clone",
@@ -12,10 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+    
+  )
 }
