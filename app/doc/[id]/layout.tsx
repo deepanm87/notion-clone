b9ffe89d2db14972c594d146a3ad77c9ad08a01d
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-10-02 16:08:49 -0400

    setup liveblocks

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -1,14 +1,18 @@
 import RoomProvider from "@/components/RoomProvider"
 import { auth } from "@clerk/nextjs/server"
 
-export default function DocLayout({
+export default async function DocLayout({
   children,
   params: { id }
 } : {
   children: React.ReactNode
   params: { id: string }
 }) {
-  auth().protect()
+  const { userId } = await auth()
+
+  if (!userId) {
+    throw new Error("Unauthorized")
+  }
 
   return <RoomProvider roomId={id}>{children}</RoomProvider>
 }
