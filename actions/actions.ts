582feaa5d7bbb1260d004cef726d11930596d047
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-10-02 20:45:16 -0400

    setup auth endpoint and delete document component and shadcn ui dialog

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { adminDb } from "@/firebase-admin"
+import liveblocks from "@/lib/liveblocks"
 import { auth, currentUser } from "@clerk/nextjs/server"
 
 export async function createNewDocument() {
@@ -37,4 +38,35 @@ export async function createNewDocument() {
     })
 
   return { docId: docRef.id }
+}
+
+export async function deleteDocument(roomId: string) {
+  const { userId } = await auth()
+
+  if (!userId) {
+    throw new Error("Unauthorized")
+  }
+
+  try {
+    await adminDb.collection("documents").doc(roomId).delete()
+
+    const query = await adminDb
+      .collectionGroup("rooms")
+      .where("roomId", "==", roomId)
+      .get()
+    const batch = adminDb.batch()
+
+    query.docs.forEach(doc => {
+      batch.delete(doc.ref)
+    })
+
+    await batch.commit()
+
+    await liveblocks.deleteRoom(roomId)
+
+    return { success: true } 
+  } catch (error) {
+    console.error(error)
+    return { success: false }
+  }
 }
